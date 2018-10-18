11232559a456dc4810cdecbaac6af9c231b9a1f8
Refik ÖNAL <refikonal@gmail.com>
2018-10-18 15:14:14 +0300

    Some awesome changes

--- index.js
+++ index.js
@@ -3,13 +3,25 @@ var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var port = process.env.PORT || 3000;
 
+
+let names = ['Osman', 'Necmi', 'Haydar', 'Hebelek', 'Çükübik', 'Fikibok'];
+let assignedNames = {};
+
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
 
 io.on('connection', function(socket){
-  socket.on('chat message', function(msg){
-    io.emit('chat message', msg);
+  const id = socket.id;
+  console.log(`${id} connected !`);
+  const name = names.pop();
+  assignedNames[id] = name;
+  console.log(`${id} is know aka ${name}`);
+
+socket.on('chat message', function(msg){
+    const name = assignedNames[id]
+    io.emit('chat message', {id, name, msg});
+    console.log(`${name} typed: "${msg}"`);
   });
 });
 
